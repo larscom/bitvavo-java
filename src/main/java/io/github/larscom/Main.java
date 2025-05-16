6f837201cc0e741d6c4c23d15408a5d90337fbd5
larscom <larskniep@gmail.com>
2025-05-16 14:14:53 +0200

    fix listener

--- src/main/java/io/github/larscom/Main.java
+++ src/main/java/io/github/larscom/Main.java
@@ -1,55 +1,30 @@
 package io.github.larscom;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import io.github.larscom.ws.*;
-
-import java.net.URISyntaxException;
-import java.util.List;
 
 public class Main {
 
-    public static void main(final String[] args) {
-        final var objectMapper = new ObjectMapper();
-        objectMapper.registerModule(new Jdk8Module());
+    public static void main(final String[] args) throws InterruptedException, JsonProcessingException {
+        final var listener = new WebSocketListener();
 
-        while (true) {
-            try {
-                final var socket = new WebSocket(objectMapper);
-                if (socket.connectBlocking()) {
-                    socket.stream()
-                        .filter(Either::isLeft)
-                        .map(Either::getLeft)
-                        .subscribe(message -> {
-                            switch (message) {
-                                case final Ticker ticker -> {
-                                    System.out.println("Ticker: " + ticker);
-                                }
-                                case final Subscription subscription -> {
-                                    System.out.println("Subscribed: " + subscription);
-                                }
-                                default -> throw new IllegalStateException("Unexpected value: " + message);
-                            }
-                        });
-
-                    final var message = MessageOut.builder()
-                        .action(Action.SUBSCRIBE)
-                        .channels(List.of(Channel.builder()
-                            .name("ticker")
-                            .markets(List.of("ETH-EUR", "BTC-EUR", "POLYX-EUR", "APT-EUR", "VANRY-EUR"))
-                            .build()))
-                        .build();
-
-                    socket.send(message);
-
-                    socket.blockUntilClosed();
-                } else {
-                    Thread.sleep(1000);
+        listener.stream()
+            .filter(Either::isLeft)
+            .map(Either::getLeft)
+            .subscribe(message -> {
+                switch (message) {
+                    case final Ticker ticker -> {
+                        System.out.println("Ticker: " + ticker);
+                    }
+                    case final Subscription subscription -> {
+                        System.out.println("Subscribed: " + subscription);
+                    }
+                    default -> throw new IllegalStateException("Unexpected value: " + message);
                 }
-            } catch (final InterruptedException | URISyntaxException | JsonProcessingException e) {
-                throw new RuntimeException(e);
-            }
-        }
+            });
+
+        listener.subscribe();
+
+        Thread.currentThread().join();
     }
 }
--- src/main/java/io/github/larscom/ws/WebSocketListener.java
+++ src/main/java/io/github/larscom/ws/WebSocketListener.java
@@ -3,38 +3,56 @@ package io.github.larscom.ws;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import io.reactivex.rxjava3.core.BackpressureStrategy;
+import io.reactivex.rxjava3.core.Flowable;
+import io.reactivex.rxjava3.subjects.PublishSubject;
 
 import java.net.URISyntaxException;
 import java.util.List;
+import java.util.concurrent.CountDownLatch;
 
 public class WebSocketListener {
     private boolean running = false;
     private WebSocket webSocket;
 
+    private final CountDownLatch startLatch;
     private final ObjectMapper objectMapper;
+    private final PublishSubject<Either<MessageIn, Error>> messagePublisher;
 
-    public WebSocketListener() {
+    public WebSocketListener() throws InterruptedException {
         objectMapper = new ObjectMapper();
         objectMapper.registerModule(new Jdk8Module());
+
+        this.messagePublisher = PublishSubject.create();
+        this.startLatch = new CountDownLatch(1);
+
+        startBlocking();
+    }
+
+    public Flowable<Either<MessageIn, Error>> stream() {
+        return messagePublisher.toFlowable(BackpressureStrategy.BUFFER);
     }
 
     public void subscribe() throws JsonProcessingException {
-        final var message = MessageOut.builder()
-            .action(Action.SUBSCRIBE)
-            .channels(List.of(Channel.builder()
-                .name("ticker")
-                .markets(List.of("ETH-EUR", "BTC-EUR", "POLYX-EUR", "APT-EUR", "VANRY-EUR"))
-                .build()))
-            .build();
+        if (running) {
+            final var message = MessageOut.builder()
+                .action(Action.SUBSCRIBE)
+                .channels(List.of(Channel.builder()
+                    .name("ticker")
+                    .markets(List.of("ETH-EUR", "BTC-EUR", "POLYX-EUR", "APT-EUR", "VANRY-EUR"))
+                    .build()))
+                .build();
 
-        webSocket.send(message);
+            webSocket.send(message);
+        }
     }
 
-    public void start() {
-        if (running) {
-            return;
-        }
+    public void close() {
+        running = false;
+        webSocket.terminate();
+    }
 
+    private void startBlocking() throws InterruptedException {
         running = true;
 
         new Thread(() -> {
@@ -42,6 +60,8 @@ public class WebSocketListener {
                 try {
                     webSocket = new WebSocket(objectMapper);
                     if (webSocket.connectBlocking()) {
+                        startLatch.countDown();
+                        webSocket.stream().subscribe(messagePublisher::onNext);
                         webSocket.blockUntilClosed();
                     } else {
                         Thread.sleep(2000);
@@ -51,5 +71,7 @@ public class WebSocketListener {
                 }
             }
         }).start();
+
+        startLatch.await();
     }
 }
